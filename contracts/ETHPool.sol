92a16d5be9885d6c7c72bf327089c64a237bc102
VMasfer <93734576+VMasfer@users.noreply.github.com>
2022-03-02 14:28:32 -0300

    feat: implemented O(1) complexity

--- contracts/ETHPool.sol
+++ contracts/ETHPool.sol
@@ -5,21 +5,14 @@ import '@openzeppelin/contracts/access/Ownable.sol';
 import './IETHPool.sol';
 
 contract ETHPool is IETHPool, Ownable {
-  PoolReward[] public poolRewards;
   mapping(address => UserDeposit) public userToDeposit;
-  mapping(address => uint256) public userToTotalETHDeposited;
-  mapping(address => uint256) public userToTotalETHWithdrawn;
-  mapping(address => uint256) private _userToTotalRewards;
-  uint256 public totalUserETHDeposited;
-  uint256 public totalUserETHWithdrawn;
-  uint256 public totalPoolRewards;
-  uint256 public ethPool;
-  uint256 public week;
+  uint256 public rewardsPerToken;
   uint256 public rewardTime;
+  uint256 public ethPool;
 
-  event UserETHDeposited(address indexed _user, uint256 _deposit, uint256 indexed _week, uint256 _date);
-  event UserETHWithdrawn(address indexed _withdrawer, uint256 _value, uint256 indexed _week, uint256 _date);
-  event PoolRewardDeposited(address indexed _team, uint256 _reward, uint256 _ethPool, uint256 _week, uint256 _date);
+  event UserETHDeposited(address indexed _user, uint256 _deposit, uint256 _date);
+  event UserETHWithdrawn(address indexed _user, uint256 _userETH, uint256 _date);
+  event PoolRewardDeposited(address indexed _team, uint256 _reward, uint256 _ethPool, uint256 _date);
   event TeamETHReceived(address indexed _sender, uint256 _value);
   event TeamETHWithdrawn(address indexed _withdrawer, uint256 _value);
 
@@ -33,63 +26,34 @@ contract ETHPool is IETHPool, Ownable {
 
   function depositUserETH() external payable override {
     require(msg.value != 0, 'No ether sent to deposit');
-    uint256 userRewards = getUserRewards(msg.sender);
-    uint256 userETH = userToDeposit[msg.sender].deposit + userRewards;
-    UserDeposit memory userDeposit;
-    userDeposit.user = msg.sender;
-    userDeposit.deposit = userETH + msg.value;
-    userDeposit.week = week;
-    userDeposit.date = block.timestamp;
+    UserDeposit memory userDeposit = userToDeposit[msg.sender];
+    userDeposit.deposit += msg.value;
+    userDeposit.rewardsPerTokenCredited = rewardsPerToken;
+    userDeposit.unclaimedRewards = getUserRewards(msg.sender);
     userToDeposit[msg.sender] = userDeposit;
-    userToTotalETHDeposited[msg.sender] += msg.value;
-    totalUserETHDeposited += msg.value;
     ethPool += msg.value;
-    _userToTotalRewards[msg.sender] += userRewards;
-    emit UserETHDeposited(msg.sender, msg.value, week, block.timestamp);
+    emit UserETHDeposited(msg.sender, msg.value, block.timestamp);
   }
 
-  function withdrawUserETH(uint256 _value) external override {
+  function withdrawUserETH() external override {
+    uint256 userDepositValue = userToDeposit[msg.sender].deposit;
+    uint256 userETH = userDepositValue + getUserRewards(msg.sender);
     //solhint-disable-next-line
-    require(address(this).balance >= _value, 'Insufficient ether in contract balance');
-    uint256 userRewards = getUserRewards(msg.sender);
-    uint256 userETH = userToDeposit[msg.sender].deposit + userRewards;
+    require(address(this).balance >= userETH, 'Insufficient ether in contract balance');
+    ethPool -= userDepositValue;
+    delete userToDeposit[msg.sender];
+    emit UserETHWithdrawn(msg.sender, userETH, block.timestamp);
     //solhint-disable-next-line
-    require(userETH >= _value, 'Insufficient ether in user balance');
-    if (userETH == _value) {
-      delete userToDeposit[msg.sender];
-    } else {
-      UserDeposit memory userDeposit;
-      userDeposit.user = msg.sender;
-      userDeposit.deposit = userETH - _value;
-      userDeposit.week = week;
-      userDeposit.date = block.timestamp;
-      userToDeposit[msg.sender] = userDeposit;
-    }
-    userToTotalETHWithdrawn[msg.sender] += _value;
-    totalUserETHWithdrawn += _value;
-    ethPool -= _value;
-    _userToTotalRewards[msg.sender] += userRewards;
-    emit UserETHWithdrawn(msg.sender, _value, week, block.timestamp);
-    //solhint-disable-next-line
-    (bool sent, ) = msg.sender.call{value: _value}('');
+    (bool sent, ) = msg.sender.call{value: userETH}('');
     require(sent, 'Failed to send ether');
   }
 
   function depositPoolReward() external payable onlyOwner {
     require(ethPool != 0, 'Nothing to reward');
     require(rewardTime <= block.timestamp, 'It has not been a week yet');
-    PoolReward memory poolReward;
-    poolReward.team = msg.sender;
-    poolReward.reward = msg.value;
-    poolReward.ethPool = ethPool;
-    poolReward.week = week;
-    poolReward.date = block.timestamp;
-    poolRewards.push(poolReward);
-    emit PoolRewardDeposited(msg.sender, msg.value, ethPool, week, block.timestamp);
-    totalPoolRewards += msg.value;
-    ethPool += msg.value;
-    week++;
+    rewardsPerToken += msg.value / ethPool;
     rewardTime = block.timestamp + 1 weeks;
+    emit PoolRewardDeposited(msg.sender, msg.value, ethPool, block.timestamp);
   }
 
   function withdrawTeamETH(uint256 _value) external onlyOwner {
@@ -99,10 +63,6 @@ contract ETHPool is IETHPool, Ownable {
     //solhint-disable-next-line
     (bool sent, ) = msg.sender.call{value: _value}('');
     require(sent, 'Failed to send ether');
-  }
-
-  function getPoolRewards() external view override returns (PoolReward[] memory) {
-    return poolRewards;
   }
 
   function getTeamETH() external view override returns (uint256) {
@@ -115,20 +75,10 @@ contract ETHPool is IETHPool, Ownable {
     return userETH;
   }
 
-  function getUserTotalRewards(address _user) external view override returns (uint256) {
-    uint256 userTotalRewards = _userToTotalRewards[_user] + getUserRewards(_user);
-    return userTotalRewards;
-  }
-
   function getUserRewards(address _user) public view override returns (uint256) {
-    uint256 userRewards;
-    if (userToDeposit[_user].deposit != 0) {
-      for (uint256 i = userToDeposit[_user].week; i < poolRewards.length; i++) {
-        uint256 poolPercentage = ((userToDeposit[_user].deposit + userRewards) * 100) / poolRewards[i].ethPool;
-        uint256 userReward = (poolRewards[i].reward * poolPercentage) / 100;
-        userRewards += userReward;
-      }
-    }
+    UserDeposit memory userDeposit = userToDeposit[_user];
+    uint256 userRewardsPerToken = rewardsPerToken - userDeposit.rewardsPerTokenCredited;
+    uint256 userRewards = userRewardsPerToken * userDeposit.deposit + userDeposit.unclaimedRewards;
     return userRewards;
   }
 }
